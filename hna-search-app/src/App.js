a9d6c23fe9c24047d0fcba2faad1d16840fb8746
Jason Coleman <jayemcee121@gmail.com>
2020-07-29 11:17:10 -0400

    initial commit

---
+++ hna-search-app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div style={{textAlign: "center"}}>
+      <h1>Search Hacker News here!</h1>
+      <form placeholder="search">
+
+      </form>
+    </div>
+  );
+}
+
+export default App;
