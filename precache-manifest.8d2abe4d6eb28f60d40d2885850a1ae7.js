42d8c26557065b44ed7fc94104437b658e3427a4
Jason Coleman <jayemcee121@gmail.com>
2020-08-05 17:23:34 -0400

    Updates

---
+++ precache-manifest.8d2abe4d6eb28f60d40d2885850a1ae7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60f24e8f4d0964adc588d73ad7264441",
+    "url": "/HNA-Search-App/index.html"
+  },
+  {
+    "revision": "9ec0eb50138bdb037a77",
+    "url": "/HNA-Search-App/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "899b8e04a24f8c770f40",
+    "url": "/HNA-Search-App/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9ec0eb50138bdb037a77",
+    "url": "/HNA-Search-App/static/js/2.85fc9e09.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/HNA-Search-App/static/js/2.85fc9e09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "899b8e04a24f8c770f40",
+    "url": "/HNA-Search-App/static/js/main.1d24b8f7.chunk.js"
+  },
+  {
+    "revision": "3a20ef80828a215a9f73",
+    "url": "/HNA-Search-App/static/js/runtime-main.42cc4cea.js"
+  }
+]);
